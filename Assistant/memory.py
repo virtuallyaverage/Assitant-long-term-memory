4fc010b97b5165416555302ae3daf2d3b64d0f30
virtuallyaverage <106417579+virtuallyaverage@users.noreply.github.com>
2023-04-15 21:03:23 -0500

    get started on the new memory and speech modules

--- Assistant/memory.py
+++ Assistant/memory.py
@@ -1,64 +1,65 @@
-import numpy as np
+import sqlite3
 from sentence_transformers import SentenceTransformer
-from milvus import Milvus, IndexType, MetricType, Status
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
-from sklearn.metrics.pairwise import cosine_similarity
-
-Base = declarative_base()
-
-class Conversation(Base):
-    __tablename__ = 'conversations'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(String, ForeignKey('users.id'))
-    conversation_data = Column(String)
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(String, primary_key=True)
-    conversations = relationship('Conversation', backref='user')
+from pymilvus import connections, Collection, FieldSchema, DataType, CollectionSchema
 
 class LongTermMemory:
     def __init__(self):
         self.setup_memory_storage()
         self.vectorizer = SentenceTransformer('bert-base-nli-mean-tokens')
-        self.milvus_client = Milvus(host='localhost', port='19530')
+        self.setup_milvus_connection()
 
     def setup_memory_storage(self):
-        self.engine = create_engine('sqlite:///memory.db')
-        Base.metadata.create_all(self.engine)
-        self.Session = sessionmaker(bind=self.engine)
-        self.session = self.Session()
+        self.conn = sqlite3.connect('data/memory.db')
+        self.cursor = self.conn.cursor()
 
-    def retrieve_past_conversations(self, user_identifier):
-        try:
-            past_conversations = self.session.query(Conversation).filter_by(user_id=user_identifier).all()
-            return [conv.conversation_data for conv in past_conversations]
-        except Exception as e:
-            print("Error:", e)
-            return []
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id TEXT PRIMARY KEY
+        )''')
+
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS conversations (
+            id INTEGER PRIMARY KEY,
+            user_id TEXT,
+            conversation_data TEXT,
+            FOREIGN KEY (user_id) REFERENCES users (id)
+        )''')
+
+    def setup_milvus_connection(self):
+        connections.connect()
+        fields = [
+            FieldSchema(name="id", dtype=DataType.INT64, is_primary=True),
+            FieldSchema(name="vector", dtype=DataType.FLOAT_VECTOR, dim=768)
+        ]
+        schema = CollectionSchema(fields=fields, description="Conversations vector storage", auto_id=False)
+        vector_index = {"index_type": "IVF_SQ8", "metric_type": "COSINE", "params": {"nlist": 100}}
+        self.collection = Collection(name="conversations", schema=schema)
+        self.collection.create_index(field_name="vector", index_params=vector_index)
 
     def store_conversation(self, user_identifier, conversation_data):
         try:
-            new_conversation = Conversation(user_id=user_identifier, conversation_data=conversation_data)
-            self.session.add(new_conversation)
-            self.session.commit()
+            self.cursor.execute("INSERT OR IGNORE INTO users (id) VALUES (?)", (user_identifier,))
+            self.cursor.execute("INSERT INTO conversations (user_id, conversation_data) VALUES (?, ?)", (user_identifier, conversation_data))
+            self.conn.commit()
+
+            conversation_id = self.cursor.lastrowid
+            conversation_vector = self.vectorizer.encode([conversation_data])[0]
+            self.collection.insert([{"id": conversation_id, "vector": conversation_vector.tolist()}])
         except Exception as e:
             print("Error:", e)
 
     def find_similar_conversations(self, user_identifier, input_text, top_n=5):
-        past_conversations = self.retrieve_past_conversations(user_identifier)
-        if not past_conversations:
-            return []
-
         input_vector = self.vectorizer.encode([input_text])[0]
-        past_vectors = self.vectorizer.encode(past_conversations)
-        similarities = cosine_similarity([input_vector], past_vectors)[0]
-        top_indices = np.argsort(similarities)[-top_n:][::-1]
+        search_params = {"metric_type": "COSINE", "params": {"nprobe": 10}}
+        results = self.collection.search([input_vector.tolist()], anns_field="vector", top_k=top_n, search_params=search_params)
 
-        return [past_conversations[i] for i in top_indices]
+        top_indices = [hit.id for hit in results[0]]
+        self.cursor.execute("SELECT conversation_data FROM conversations WHERE id IN ({})".format(','.join('?' * len(top_indices))), top_indices)
+        similar_conversations = [row[0] for row in self.cursor.fetchall()]
+
+        return similar_conversations
 
     def close(self):
-        self.session.close()
+        self.conn.close()
+        connections.disconnect()
 
---
+++ Assistant/speechProcessing.py
@@ -0,0 +1,89 @@
+import pyaudio
+from pvporcupine import Porcupine
+from pvrhino import Rhino
+from pvleopard import Leopard
+from TTS.utils.manage import ModelManager
+from TTS.utils.synthesizer import Synthesizer
+
+class SpeechProcessing:
+    def __init__(self, access_key, keyword_path, context_path, porcupine_sensitivity, rhino_sensitivity):
+        self.leopard = Leopard(access_key)
+
+        self.model_manager = ModelManager()
+        self.tts_model, self.tts_config = self.model_manager.download_model("tts_models/en/ljspeech/tacotron2-DDC")
+        self.tts_synthesizer = Synthesizer(self.tts_model, self.tts_config)
+
+        def wake_word_callback():
+            pass
+        
+        self.porcupine = Porcupine(keyword_path=keyword_path, sensitivity=porcupine_sensitivity)
+        self.rhino = Rhino(context_path=context_path, sensitivity=rhino_sensitivity)
+
+    def convert_speech_to_text(self, audio_input):
+        try:
+            text_output = self.leopard.transcribe(audio_input)
+            return text_output
+        except Exception as e:
+            print(f"Error in convert_speech_to_text: {e}")
+            return ""
+
+    def convert_text_to_speech(self, text_input):
+        try:
+            audio_output = self.tts_synthesizer.tts(text_input)
+            return audio_output
+        except Exception as e:
+            print(f"Error in convert_text_to_speech: {e}")
+            return None
+
+    def delete(self):
+        self.porcupine.delete()
+        self.rhino.delete()
+        self.leopard.delete()
+
+
+import wave
+import time
+
+if __name__ == "__main__":
+    access_key = "YOUR_ACCESS_KEY"
+    keyword_path = "path/to/keyword.ppn"
+    context_path = "path/to/context.rhn"
+    porcupine_sensitivity = 0.5
+    rhino_sensitivity = 0.5
+
+    speech_processing = SpeechProcessing(access_key, keyword_path, context_path, porcupine_sensitivity, rhino_sensitivity)
+
+    # Test speech-to-text using Leopard
+    audio_input = "path/to/audio.wav"
+    text_output = speech_processing.convert_speech_to_text(audio_input)
+    print(f"Speech-to-text output: {text_output}")
+
+    # Test text-to-speech using Coqui TTS
+    text_input = "Hello, this is a test message."
+    audio_output = speech_processing.convert_text_to_speech(text_input)
+
+    # Save the generated audio to a WAV file
+    with wave.open("output.wav", "wb") as wav_file:
+        wav_file.setparams((1, 2, 22050, 0, "NONE", "not compressed"))
+        wav_file.writeframes(audio_output)
+
+    # Test Porcupine and Rhino processing
+    # Replace this line with the actual audio frames from your audio input source
+    audio_frames = [b'\x00\x00' * 512] * 100
+
+    for frame in audio_frames:
+        try:
+            result = speech_processing.porcupine.process(frame)
+            if result:
+                print("Wake word detected")
+                # Process audio frames with Rhino after wake word detection
+                is_finalized = speech_processing.rhino.process(frame)
+                if is_finalized:
+                    inference = speech_processing.rhino.get_inference()
+                    print(f"Intent: {inference['intent']}, Slots: {inference['slots']}")
+
+        except Exception as e:
+            print(f"Error processing audio frame: {e}")
+            break
+
+    speech_processing.delete()
